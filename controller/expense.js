84d450f64fd4ff13ee7c12c7a43271cfe9cba005
Shubham4206 <ishubham803213@gmail.com>
2023-06-01 18:22:19 +0530

    downloadexpense

---
+++ controller/expense.js
@@ -0,0 +1,99 @@
+const Expense=require('../model/expense');
+const User=require('../model/user'); 
+//const { Op } = require('sequelize');
+const sequelize = require('../util/database');
+
+
+exports.addExpense=async(req,res,next)=>{
+    const{amount,description,category}=req.body;
+    if(amount.length>0 && description.length>0 && category.length>0){
+        try{
+            const t=await sequelize.transaction();
+        const totalexpense=Number(req.user.totalExpense)+Number(amount);
+        await User.update({totalExpense:totalexpense},
+            {where:{id:req.user.id}},
+            {transaction:t});
+      let data = await  req.user.createExpense({
+                amount:amount,
+                description:description,
+                category:category
+            },
+            {transaction:t} );
+           // console.log(data);
+          await t.commit();
+         res.status(200).json(data);
+        
+    } catch (error) {
+        t.rollback();
+        res.status(500).json({success: false, message: error});
+    }
+    }
+}
+
+exports.getExpense=async(req,res,next)=>{
+    try{
+        let data= await req.user.getExpenses();
+        res.status(200).json(data);
+    }catch(err){
+        res.status(500).json({success:false,message:err});
+    }
+}
+
+exports.deleteExpense=async(req,res,next)=>{
+    const t=await sequelize.transaction();
+    const uid=req.params.id;
+    const amount=req.params.amount;
+    const totalexpense=Number(req.user.totalExpense)-Number(amount);
+    await Expense.destroy({
+        where:{
+         id:uid
+        }
+        
+    },{transaction:t});
+    await User.update({totalExpense:totalexpense},
+        {where:{id:req.user.id}},
+        {transaction:t});
+    res.sendStatus(200);
+
+}
+
+exports.getLeaderboard=async(req,res,next)=>{
+    if(req.user.isPremiumUser === true) {
+        try{
+         let user=await User.findAll({
+            attributes:['id','name','totalExpense']
+         });
+            user.sort((a,b)=>b.totalExpense-a.totalExpense);
+                    res.status(200).json(user);
+                } catch (error) {
+                    throw new Error(error);
+                }
+    } else {
+        res.status(403).json({success: false, message: 'user does not premium membership'});
+    }
+
+};
+
+
+exports.downloadExpense = async (req, res) => {
+    if(req.user.isPremiumUser) {
+        try {
+            const expenses = await req.user.getExpenses();
+            console.log(expenses);
+                                                //  file name  //                           //  data    //
+            const fileUrl = await uploadToS3(`${req.user.id}_${new Date()}_expenses.csv`, JSON.stringify(expenses));
+
+            // console.log('fileUrl>>>>>', fileUrl);
+            await req.user.createDownload({fileUrl: fileUrl, date: new Date()});
+
+            res.status(201).json({fileUrl: fileUrl, success: true});
+            
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({error, status: false});
+        }
+
+    }else {
+        res.status(401).json({success: false, message: 'user does not have Premium Membership'});
+    }
+}
